cde8bc97d98764111b8921a94eb92e57b9ce8f6a
chandicharan5983 <chandicharan5983@gmail.com>
2022-03-01 22:30:25 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'CCP ' + VERSION.major;
+  name = 'CCP CCCC ' + VERSION.major;
 }
